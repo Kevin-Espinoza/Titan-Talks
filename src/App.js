16739e94b12b3eb951be071adf8856be775af868
Kevin Espinoza <k.espinoza1012@csu.fullerton.edu>
2021-10-14 13:40:43 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ function App() {
   <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
     <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0"></div>
     <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-      <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Welcome to Titan Talks</h2>
+      <h2 class="text-gray-900 text-lg font-medium title-font mb-5">Sign Up</h2>
       <div class="relative mb-4">
-        <label for="full-name" class="leading-7 text-sm text-gray-600">Username</label>
+        <label for="full-name" class="leading-7 text-sm text-gray-600">Full Name</label>
         <input type="text" id="Username" name="Username" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
+        
       </div>
       <div class="relative mb-4">
-        <label for="email" class="leading-7 text-sm text-gray-600">Password</label>
+        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
         <input type="email" id="Password" name="Password" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
+        
       </div>
       <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
       
